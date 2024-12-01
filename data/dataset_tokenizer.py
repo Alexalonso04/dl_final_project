419f368478c1027dde67e515041c899d0419e927
Alejandro Alonso Garcia <agarcia349@gatech.edu>
2024-11-30 19:46:21 -0500

    fix(multiprocessing): fixed pool multiprocessing by inserting it inside __main__ and loading the dataset beforehand

--- data/dataset_tokenizer.py
+++ data/dataset_tokenizer.py
@@ -9,10 +9,6 @@ from tqdm import tqdm
 SHARD_SIZE = int(1e8) # 100M
 SHARD_DIR = pathlib.Path("data/shards")
 
-# use name="sample-10BT" to use the 10BT sample of fineweb dataset
-# use multiple CPU processes to speed up the download
-raw_dataset = load_dataset("HuggingFaceFW/fineweb", name="sample-10BT", num_proc=cpu_count() - 4)
-
 # Tokenizer Initialization
 ENCODING = tiktoken.get_encoding("gpt2")
 EOT = ENCODING._special_tokens['<|endoftext|>'] # end of text token
@@ -23,10 +19,10 @@ def tokenizer(doc):
     We're using uint16 to save on memory and computation
     as this is enough precision for GPT2
     """
-    tokens = [EOT]
-    tokens.extend(ENCODING.encode_ordinary(doc["text"]))
+    token_list = [EOT]
+    token_list.extend(ENCODING.encode_ordinary(doc["text"]))
 
-    tokens_array = np.array(tokens, dtype=np.uint16)
+    tokens_array = np.array(token_list, dtype=np.uint16)
     # Create a test case for this
     # assert (tokens_array >= 0).all() and (tokens_array < 2**16).all(), "Tokens must not exceed uint16 bytes"
 
@@ -37,39 +33,43 @@ def save_data(token_array: np.ndarray, shard_index: int):
     Save an array of tokens into a binary file to save on space
     and memoryt
     """
-    shard_split = "val" if shard_idx == 0 else "train"
+    shard_split = "val" if shard_index == 0 else "train"
     file = f"shard-{shard_split}-{shard_index:06d}"
     np.save(SHARD_DIR.joinpath(file), token_array)
 
-with Pool(max(1, cpu_count()//2)) as pool:
-    shard_idx = 0
+if __name__ == '__main__':
+    raw_dataset = load_dataset("HuggingFaceFW/fineweb", name="sample-10BT")
 
-    # Create an empty array for the tokens of a single shard
-    shard_tokens = np.empty((SHARD_SIZE,), dtype=np.uint16)
-    token_count = 0
+    with Pool(max(1, cpu_count()//2)) as pool:
+        shard_idx = 0
+        progress_bar = None
 
-    for tokens in pool.imap(tokenizer, raw_dataset, chunksize=16):
-        
-        # If the shard is full of tokens, save it,
-        # and move onto the next shard
-        if token_count + len(tokens) >= SHARD_SIZE:
-            remainder = SHARD_SIZE - token_count
-            shard_tokens[token_count:token_count+remainder] = tokens[:remainder]
-            save_data(shard_tokens, shard_idx)
+        # Create an empty array for the tokens of a single shard
+        shard_tokens = np.empty((SHARD_SIZE,), dtype=np.uint16)
+        token_count = 0
 
-            shard_tokens[0:len(tokens)-remainder] = tokens[remainder:]
-            token_count = len(tokens) - remainder
-            shard_idx += 1
-            progress_bar = None
-            continue
-        
-        # Fill tokens into the current non-empty shard
-        shard_tokens[token_count:token_count+len(tokens)] = tokens
-        token_count += len(tokens)
-        if progress_bar is None:
-            progress_bar = tqdm(total=SHARD_SIZE, unit="tokens", desc=f"Shard {shard_idx}")
-        progress_bar.update(len(tokens))
-    
-    # Save whatever tokens remain into the last shard
-    if token_count != 0:
-        save_data(shard_tokens[:token_count], shard_idx)
+        for tokens in pool.imap(tokenizer, raw_dataset['train'], chunksize=16):
+
+            # If the shard is full of tokens, save it,
+            # and move onto the next shard
+            if token_count + len(tokens) >= SHARD_SIZE:
+                remainder = SHARD_SIZE - token_count
+                shard_tokens[token_count:token_count+remainder] = tokens[:remainder]
+                save_data(shard_tokens, shard_idx)
+
+                shard_tokens[0:len(tokens)-remainder] = tokens[remainder:]
+                token_count = len(tokens) - remainder
+                shard_idx += 1
+                progress_bar = None
+                continue
+            
+            # Fill tokens into the current non-empty shard
+            shard_tokens[token_count:token_count+len(tokens)] = tokens
+            token_count += len(tokens)
+            if progress_bar is None:
+                progress_bar = tqdm(total=SHARD_SIZE, unit="tokens", desc=f"Shard {shard_idx}")
+            progress_bar.update(len(tokens))
+
+        # Save whatever tokens remain into the last shard
+        if token_count != 0:
+            save_data(shard_tokens[:token_count], shard_idx)
