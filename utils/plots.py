9e527c66c51233f02128adf86f391a3f38c00229
Alejandro <alexalonso04@gmail.com>
2024-12-09 21:15:12 -0500

    added utilities for the plots

--- utils/plots.py
+++ utils/plots.py
@@ -14,38 +14,41 @@ def log_extractor(path: str) -> tuple[list[np.int16], list[np.float16], list[np.
         log = f.read()
 
     # Define the regular expression
-    info_pattern = r"(?<=step:)(?P<step>\d+).+(?<=train_loss:)(?P<loss>\d+.\d+).+(?<=train_time:)(?P<time>\d+).+(?<=step_avg:)(?P<step_time>\d+.\d+)"
+    info_pattern = r"(?<=Step\s)(?P<step>\d+):\strain_loss=(?P<train_loss>\d+.\d+),\stime=(?P<step_time>\d+.\d+).+lr=(?P<learning_rate>\d+.\d+[e\-+]+\d+)"
 
     # Find all matches
     matches = re.finditer(info_pattern, log)
 
-    steps, losses, times, step_times = [], [], [], []
+    steps, losses, times, lr_array = [], [], [], []
 
     for info in matches:
         step = np.int16(info.group("step"))
-        loss = np.float16(info.group("loss"))
-        time = np.float16(info.group("time"))
+        loss = np.float16(info.group("train_loss"))
         step_time = np.float16(info.group("step_time"))
+        learning_rate = np.float16(info.group("learning_rate"))
 
         steps.append(step)
         losses.append(loss)
-        times.append(time)
-        step_times.append(step_time)
+        times.append(step_time)
+        lr_array.append(learning_rate)
     
-    return (steps, losses, times, step_times)
+    return (steps, losses, times, lr_array)
 
-def plot_loss_vs_steps(steps: list[np.int16], losses: list[np.float16]) -> None:
+def plot_loss_vs_steps(steps: list[np.int16], steps_a, losses: list[np.float16], losses_a) -> None:
     """
     Plots the loss vs. steps.
     """
     # Calculate average loss for each method
     avg_loss = np.mean(np.array(losses))
+    avg_loss_a = np.mean(np.array(losses_a))
 
     # Plot the data
-    plt.plot(steps, losses, '-o', color='orange', label='Modified NanoGTP', linewidth=2, markersize=6)
+    plt.plot(steps[::128], losses[::128], '-o', color='orange', label='Modified NanoGTP', linewidth=2, markersize=6)
+    plt.plot(steps_a[::128], losses_a[::128], '-o', color='black', label='Base NanoGTP', linewidth=2, markersize=6)
 
     # Add horizontal lines for average loss
     plt.axhline(y=avg_loss, color='orange', linestyle='dotted', linewidth=1, label='Avg Loss (Modified NanoGPT)')
+    plt.axhline(y=avg_loss_a, color='black', linestyle='dotted', linewidth=1, label='Avg Loss (Base NanoGPT)')
 
     # Customize the plot
     plt.xlabel('Timesteps', fontsize=12)
@@ -61,5 +64,6 @@ def plot_loss_vs_steps(steps: list[np.int16], losses: list[np.float16]) -> None:
     plt.tight_layout()
     plt.show()
 
-step_list, loss_list, times, step_times = log_extractor(path="logs.txt")
-plot_loss_vs_steps(step_list, loss_list)
+step_list, loss_list, times, step_times = log_extractor(path="diff_losses.log")
+step_list_a, loss_list_a, times, step_times = log_extractor(path="base_losses.log")
+plot_loss_vs_steps(step_list, step_list_a, loss_list, loss_list_a)
